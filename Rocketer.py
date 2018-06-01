23c47a1c1763d26f89ffaf7a338c8f5bed399788
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-01 18:17:06 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -244,8 +244,9 @@ async def on_member_remove(member):
 async def on_message(message):
     if message.content.startswith('r-lock'):
         if message.author.id in Moderators:
+            pr_server = get_server(id="370269066864361472")
             room = message.channel
-            Registered = discord.utils.find(lambda m: m.name == 'Registered', channel.server.roles)
+            Registered = discord.utils.find(lambda m: m.name == 'Registered', pr_server.roles)
             overwrite = discord.Overwrite()
             overwrite.send_messages = False
             await bot.edit_channel_permissions(room, Registered, overwrite)
