f3bc054bff6361812ffc092b39f918be88326f57
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-24 20:52:55 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -37,13 +37,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret.
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
@@ -64,8 +59,7 @@ def channels_list_by_username(service, **kwargs):
         results['items'][0]['statistics']['viewCount']))
 
 if __name__ == '__main__':
-  # When running locally, disable OAuthlib's HTTPs verification. When
-  # running in production *do not* leave this option enabled.
+
   os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
   service = get_authenticated_service()
   channels_list_by_username(service,
