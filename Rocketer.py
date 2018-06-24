d4f1ac628076d22915871db00957d8bcda111778
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-24 14:32:03 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -426,14 +426,14 @@ async def verify(ctx):
 @bot.event
 async def on_channel_create(channel):
     channelsroom = bot.get_channel(id="460397552379101184")
-    channels = await bot.get_all_channels()
+    channels = bot.get_all_channels()
     channels = int(channels)
     await bot.edit_channel(channelsroom, name=f"🌐Channels: {channels}")
 
 @bot.listen()
 async def on_member_join(member):
     membersroom = bot.get_channel(id="460397271788421120")
-    members = await bot.get_all_members()
+    members = bot.get_all_members()
     members = int(members)
     await bot.edit_channel(membersroom, name=f"👤Members: {members}")
     room2 = bot.get_channel(id="370269066864361476")
