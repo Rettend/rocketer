2bd855b460e8ca9cdd7c9670d8a610ff2daa2807
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-25 08:00:48 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -31,41 +31,6 @@ async def on_ready():
 class NoPermError(Exception):
     pass
 
-import google.oauth2.credentials
-
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-CLIENT_SECRETS_FILE = "client_secret.json"
-
-SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
-API_SERVICE_NAME = 'youtube'
-API_VERSION = 'v3'
-
-def get_authenticated_service():
-  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_console()
-  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
-
-def channels_list_by_username(service, **kwargs):
-  results = service.channels().list(
-    **kwargs
-  ).execute()
-  
-  print('This channel\'s ID is %s. Its title is %s, and it has %s views.' %
-       (results['items'][0]['id'],
-        results['items'][0]['snippet']['title'],
-        results['items'][0]['statistics']['viewCount']))
-
-if __name__ == '__main__':
-
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-  service = get_authenticated_service()
-  channels_list_by_username(service,
-      part='snippet,contentDetails,statistics',
-      forUsername='GoogleDevelopers')
-
 """@bot.command()
 async def load(extension_name : str):
     try:
